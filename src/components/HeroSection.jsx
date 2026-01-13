df31f4fd7dc1698a16b9cdfe67fb983375a235d4
Ronik Joshi <ronikjoshi98@gmail.com>
2026-01-13 16:27:04 +0000

    added x and menu lucide icon

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,12 @@
 
-import { HomeIcon } from 'lucide-react';
+import { HomeIcon, Menu, X } from 'lucide-react';
 import heroImage from '../assets/hero.png';
+import { useState } from 'react';
 
 const HeroSection = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const navItems = [
     {name: "Home", link: "#home"},
     {name: "About", link: "#about"},
@@ -18,35 +22,50 @@ const HeroSection = () => {
       className='absolute inset-0 bg-cover bg-center bg-no-repeat'
       style={{backgroundImage: `url(${heroImage})`}}
       >
-        <div className='flex items-center justify-between max-w-7xl mx-auto px-6'> {/* flex for (Home Icon and Education),(nav bar), (CTA) */}
-          <div className='flex items-center space-x-3'> {/* flex for Home Icon and Education */}
-            <div className='p-2 bg-blue-500 rounded-lg'>
-              <HomeIcon className='w-6 h-6 text-white'/>
+        <nav className="w-full py-4">
+
+          <div className='flex items-center justify-between max-w-7xl mx-auto px-6'> {/* flex for (Home Icon and Education),(nav bar), (CTA) */}
+            
+            <div className='flex items-center space-x-3'> {/* flex for Home Icon and Education */}
+              <div className='p-2 bg-blue-500 rounded-lg'>
+                <HomeIcon className='w-6 h-6 text-white'/>
+              </div>
+              <span className='text-2xl font-bold text-white'>
+                Education
+              </span>
+            </div> 
+            
+            <div className='hidden md:flex items-center space-x-8'>
+              {navItems.map((item, index) => (
+                <a
+                key={item.name}
+                href={item.link}
+                className='text-white/80 hover:text-white transition-colors text-lg'
+                >
+                  {item.name}
+                </a>
+              ))}
             </div>
-            <span className='text-2xl font-bold text-white'>
-              Education
-            </span>
-          </div> 
-          <div className='hidden md:flex items-center space-x-8'>
-            {navItems.map((item, index) => (
-              <a
-              key={item.name}
-              href={item.link}
-              className='text-white/80 hover:text-white transition-colors text-lg'
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <div className='hidden md:flex items-center space-x-3'>
-            <button className='px-6 py-2 bg-white text-black font-semibold rounded-xl hover:bg-gray-200 transition-colors'>
-              Sign in
+            
+            <div className='hidden md:flex items-center space-x-3'>
+              <button className='px-6 py-2 bg-white text-black font-semibold rounded-xl hover:bg-gray-200 transition-colors'>
+                Sign in
+              </button>
+              <button className='px-6 py-2 bg-white text-black font-semibold rounded-xl hover:bg-gray-200 transition-colors'>
+                Talk to us
+              </button>
+            </div>
+
+            <button
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className='md:hidden text-white'
+            >
+              {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
             </button>
-            <button className='px-6 py-2 bg-white text-black font-semibold rounded-xl hover:bg-gray-200 transition-colors'>
-              Talk to us
-            </button>
+          
           </div>
-        </div>
+
+        </nav>  
       </div>
     </div>
   )
