ee7b2a6aa2c3b8c6377b4eb561404309b9c4c4fb
Ronik Joshi <ronikjoshi98@gmail.com>
2026-01-13 10:50:39 +0000

    added icons and making navbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+
+import { HomeIcon } from 'lucide-react';
+import heroImage from '../assets/hero.png';
 
 const HeroSection = () => {
+  const navItems = [
+    
+  ]
+
   return (
-    <div>HeroSection</div>
+    <div className='relative h-screen overflow-hidden'>
+      <div
+      className='absolute inset-0 bg-cover bg-center bg-no-repeat'
+      style={{backgroundImage: `url(${heroImage})`}}
+      >
+        <div className='flex items-center justify-between max-w-7x1 mx-auto px-6'> {/* flex for (Home Icon and Education),(nav bar), (CTA) */}
+          <div className='flex items-center space-x-3'> {/* flex for Home Icon and Education */}
+            <div className='p-2 bg-blue-500 rounded-lg'>
+              <HomeIcon className='w-6 h-6 text-white'/>
+            </div>
+            <div className='text-2xl font-bold text-white'>
+              Education
+            </div>
+          </div> 
+          <div className=''>
+
+          </div>
+          <div>CTA</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default HeroSection
+
+
+ 
+
+ 
