eb506c5a47ae97760014b57f5ba6a40a5b6d04ab
Ronik Joshi <ronikjoshi98@gmail.com>
2026-01-14 11:39:34 +0000

    hero page almost completed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 
-import { HomeIcon, Menu, X } from 'lucide-react';
+import { HomeIcon, Menu, X, ArrowRight} from 'lucide-react';
 import heroImage from '../assets/hero.png';
 import { useState } from 'react';
 
@@ -48,7 +48,7 @@ const HeroSection = () => {
             </div>
             
             <div className='hidden md:flex items-center space-x-3'>
-              <button className='px-6 py-2 bg-white text-black font-semibold rounded-xl hover:bg-gray-200 transition-colors'>
+              <button className='px-8 py-2 bg-white text-black font-semibold rounded-xl hover:bg-gray-200 transition-colors'>
                 Sign in
               </button>
               <button className='px-6 py-2 bg-white text-black font-semibold rounded-xl hover:bg-gray-200 transition-colors'>
@@ -86,15 +86,31 @@ const HeroSection = () => {
           )}
         </nav> 
 
-        <h1>
-          Build your Digital
-          <span>
-            Future With Us
-          </span>
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore fuga asperiors laborum?
-        </p>
+        <div className='text-center'>    
+          <h1
+          className='text-4xl sm:text-5xl md:text-7xl font-bold text-white mb-4 md:m-6 leading-tight'
+          >
+            Build your Digital
+            <span
+            className='block mt-2'
+            >
+              Future With Us
+            </span>
+          </h1>
+          <p
+          className='text-lg sm:text-xl text-white mb-6 md:mb-10 max-w-2xl mx-auto px-2'
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore fuga asperiors laborum?
+          </p>
+          
+          <button
+          className='block mx-auto px-6 sm:px-8 py-3 sm:py-4 bg-white text-black font-semibold rounded-xl hover:opacity-90 transition-opacity flex items-center justify-center gap-2 group mb-8 md:my-12'
+          >
+            Talk to us
+          <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform"/>
+            </button>
+          
+        </div>  
       
       </div>
     </div>
