de95b29723b99ad8c9ddf4cf1e52c64708e7cc88
Ronik Joshi <ronikjoshi98@gmail.com>
2026-02-19 14:14:34 +0000

    Basic structure and layout completed

--- src/features/home/HeroSection.jsx
+++ src/features/home/HeroSection.jsx
@@ -3,12 +3,12 @@ import heroImage from '../../assets/hero.png';
 
 const HeroSection = () => {
   return (
-      <section className='relative h-screen w-full'>
+      <section className='relative h-screen w-full flex flex-col'>
         <div 
         className='absolute inset-0 bg-cover bg-center bg-no-repeat'
         style={{backgroundImage: `url(${heroImage})`}}>
         </div>
-        <div className='flex justify-between p-6'>
+        <div className='relative z-10 flex justify-between items-center p-6 text-white'>
           <div className='flex'>
             <div>ICON And GIGA</div>
             <div>Product</div>
@@ -19,11 +19,19 @@ const HeroSection = () => {
             <div>Talk to us</div>
           </div>
         </div>
-        <div className='flex-1 flex flex-col items-center justify-center'>
+        <div className='relative z-10 flex-1 flex flex-col items-center justify-center'>
           <div>GIGA launches Browser Agent</div>
           <div>AI that talks like a human. Handles millions of calls</div>
           <div>AI agent for enterprise support</div>
           <div>Talk to us</div>
+        </div>
+        <div className='relative z-10 flex justify-between items-center p-6'>
+          <div>Postman</div>
+          <div>Rio</div>
+          <div>DoorDash</div>
+          <div>Capital.com</div>
+          <div>afriex</div>
+          <div>sendoso</div>
         </div>
       </section>
   )
